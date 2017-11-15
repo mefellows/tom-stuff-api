3d3f80d2dd0eedc6249069a9ef473fb9098aa599
Matt Fellows <matt.fellows@onegeek.com.au>
2017-11-15 21:19:59 +1100

    Add basic routes for API

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/todos', (req, res) => {
+  res.send(`TODO: list of todos to go here`)
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params['id'];
+  
+  res.send(`TODO: single todo ${id} goes here`)
+})
+
+app.post('/todos', (req, res) => {
+  res.send(`TODO: create todo goes here`)
+})
+
+app.put('/todos/:id', (req, res) => {
+  const id = req.params['id'];
+  
+  res.send(`TODO: update single todo ${id} goes here`)
+})
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params['id'];
+  
+  res.send(`TODO: update single todo ${id} goes here`)
+})
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
