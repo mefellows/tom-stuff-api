a00f47d7c2f7e9d9e34402dfc7744a5b51fe798b
Matt Fellows <matt.fellows@onegeek.com.au>
2017-11-26 19:07:20 +1100

    feat(db): retrieve data from DB

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const app = express()
 
+// Setup DB connection
+const pgp = require('pg-promise')();
+const connection = {
+  host: 'localhost',
+  port: 5432,
+  database: 'postgres',
+  user: 'postgres',
+  password: 'password'
+};
+const db = pgp(connection);
+
 app.get('/todos', (req, res) => {
-  res.send(`TODO: list of todos to go here`)
+  // Select all tasks from db and return as JSON
+  return db.any('SELECT * FROM tasks')
+    .then((data) => {
+      return res.status(200).send(data)
+    })
+    .catch((error) => {
+      return res.status(500)
+    });
 })
 
 app.get('/todos/:id', (req, res) => {
   const id = req.params['id'];
-  
-  res.send(`TODO: single todo ${id} goes here`)
+
+  // Select task from db where id = 1 and return as JSON
+  return db.any('SELECT * FROM tasks WHERE id = $1', [parseInt(id)])
+    .then((data) => {
+      return res.status(200).send(data)
+    })
+    .catch((error) => {
+      return res.status(404)
+    });
 })
 
 app.post('/todos', (req, res) => {
@@ -17,13 +42,13 @@ app.post('/todos', (req, res) => {
 
 app.put('/todos/:id', (req, res) => {
   const id = req.params['id'];
-  
+
   res.send(`TODO: update single todo ${id} goes here`)
 })
 
 app.delete('/todos/:id', (req, res) => {
   const id = req.params['id'];
-  
+
   res.send(`TODO: update single todo ${id} goes here`)
 })
 
